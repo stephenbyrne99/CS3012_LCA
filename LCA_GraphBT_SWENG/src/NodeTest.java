a4fb7cc3c29fecdfddc02c75588b8bbce2abccf4
Stephen Byrne <byrnes68@tcd.ie>
2019-09-17 16:34:59 +0100

    file didn't commit on last commit

--- LCA_GraphBT_SWENG/src/NodeTest.java
+++ LCA_GraphBT_SWENG/src/NodeTest.java
@@ -2,6 +2,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
+/*
+ * 	Probably overkill testing these so thoroughly but doing it to revise j unit tests before writing LCA tests.
+ * 
+ */
 class NodeTest {
 
 	@Test
@@ -9,12 +14,20 @@ class NodeTest {
 		assert(true);
 	}
 	
+	//constr1 tests
 	@Test
 	void testConstructorNoParameters() {
 		Node node1 = new Node();
 		assertEquals(0, node1.getData());
-		assertEquals(null,node1.getLeft(), "created node not null");
-		assertEquals(null,node1.getRight(), "created node not null");
+		assertEquals(null,node1.getLeft(), "created node.left not null");
+		assertEquals(null,node1.getRight(), "created node.right not null");
 	}
+	
+	//constr2 tests
+	
+	//constr3 tests
+	
+	//methods
+	
 
 }
