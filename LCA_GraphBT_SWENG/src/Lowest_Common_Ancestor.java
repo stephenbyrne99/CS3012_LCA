a8a22d367fb486bcf1198b6f280ce80d8386394d
Stephen Byrne <byrnes68@tcd.ie>
2019-09-17 15:49:38 +0100

    Init project

---
+++ LCA_GraphBT_SWENG/src/Lowest_Common_Ancestor.java
@@ -0,0 +1,17 @@
+
+/*	
+ * 1. Choose a unit testing framework.
+ * 2. Create a repository on a git service such as Github. 
+ * 3. Define a set of initial unit tests that capture the basic expected behaviour. 
+ * 		This will require you to create sample data and also to identify the basic API for your solution.
+ * 4. Build your initial solution, testing it against your test code until it is working.
+ * 5. Refine the solution, expanding your test cases to deal with awkward parameters and edge cases that 
+ * 		push the limits of your solution, enhancing your solution to deal with these.
+ */
+
+
+
+
+public class Lowest_Common_Ancestor {
+
+}
---
+++ LCA_GraphBT_SWENG/src/Lowest_Common_AncestorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Lowest_Common_AncestorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
