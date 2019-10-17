b96c6ac51dc51e242ec6085d1b8acca4cc570603
Stephen Byrne <byrnes68@tcd.ie>
2019-10-17 23:28:50 +0100

    cont last

--- LCA_GraphBT_SWENG/src/NodeTest.java
+++ LCA_GraphBT_SWENG/src/NodeTest.java
@@ -15,34 +15,6 @@ class NodeTest {
 	void testRunning() {
 		assert(true);
 	}
-	
-	//Method tests
-	@Test 
-	void testGetNodesWhenNoNodesExist(){
-		Node one = new Node(1);
-		assertEquals(one.getNextNodes(), new ArrayList<Node>());
-	}
-	
-	/*
-	@Test
-	void testGetNextNodesGetNodeAtIndex() {
-		Node one = new Node(1);
-		Node two = new Node(2);
-		one.getNextNodes().add(two);
-		assertEquals(one.getNextNodesGetNodeAtIndex(0),two);
-	}
-	
-	@Test
-	void testGetNextNodesGetNodeAtIndexOutOfBounds() {
-		Node one = new Node(1);
-		Node two = new Node(2);
-		one.getNextNodes().add(two);
-		assertEquals(one.getNextNodesGetNodeAtIndex(1),null);
-	}
-	*/
-	
-	
-	
 
-	
+
 }
